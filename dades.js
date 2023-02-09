fc9810933c5288f1a0372c8afe6410b4ef7f4e64
ksama22 <ksama22@jaumebalmes.net>
2023-02-09 20:15:53 +0100

    Back and Next Button Song

---
+++ dades.js
@@ -0,0 +1,23 @@
+let canciones = [
+  {
+    "id": 0,
+    "titol": "The Riddle",
+    "image": "https://i1.sndcdn.com/artworks-000217816187-caa36p-t500x500.jpg",
+    "artista": "Gigi D'Agostino",
+    "path": "./audio/riddle.mp3"
+  },
+  {
+    "id": 1,
+    "titol": "Elevator",
+    "image": "https://5.imimg.com/data5/SELLER/Default/2020/12/DS/HO/NG/117137229/square-home-elevator-1000x1000.jpeg",
+    "artista": "Kevin Macleod",
+    "path": "./audio/elevator.mp3"
+  },
+  {
+    "id": 2,
+    "titol": "The Pokemon",
+    "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    "artista": "John Sieglero",
+    "path": "./audio/pokemon.mp3"
+  }
+];
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+
+var indexSong = 0;
+var audio = new Audio(canciones[indexSong]);
+var isPlaying = false;
+
 //Inicializa los segundos y minutos
 let sec = 0;
 let min = 0;
 //Esto se llama en bucle
 function bucle() {
+    if (isPlaying) {
+        goTimer();
+    }
+}
+function goTimer() {
     //Suma segundos
     sec++;
     //Se carga en pantalla
@@ -20,4 +30,60 @@ function format(numero) {
     return numero;
 }
 
-setInterval(bucle, 10);
+function playMusic() {
+    //Pausa la animacion quan no esta reproduint
+    playPauseAnimation(true)
+    isPlaying = true;
+}
+
+function backMusic() {
+    //Para la cancion anterior
+    //audio.stop();
+    //Si intenta anar una cancion mes petita retorna el minim
+    if (indexSong - 1 >= 0) {
+        indexSong--;
+    }
+    refreshData();
+}
+
+function nextMusic() {
+    //Para la cancion anterior
+    //audio.stop();
+    //Si intenta anar una cancion mes gran retorna el minim
+    if (indexSong + 1 < canciones.length) {
+        indexSong++;
+    }
+    refreshData();
+}
+
+function refreshData() {
+    let cancionOBJ = canciones[indexSong];
+    document.getElementsByClassName("title")[0].innerText = cancionOBJ.titol;
+    document.getElementsByClassName("artista")[0].innerText = cancionOBJ.artista;
+    document.getElementById("rotateImage").src = cancionOBJ.image;
+    audio = new Audio(canciones[indexSong].path);
+    console.log("Cancion actual", cancionOBJ);
+
+    // Reseteja el contador
+    sec = 0
+    min = 0;
+}
+
+function playPauseAnimation(state) {
+    if (state) {
+        for (let i = 0; i < document.getElementsByClassName("musica").length; i++) {
+            document.getElementsByClassName("musica")[i].style.animation = "musicamaestro 1s infinite";
+            //titleQuizHTML.style.animationFillMode = "forwards";
+            document.getElementsByClassName("musica")[i].style.animationDelay = i * 0.2 + "s";
+        }
+        document.getElementById("rotateImage").style.animation = "rotaMusica 5s infinite";
+    } else {
+        for (let i = 0; i < document.getElementsByClassName("musica").length; i++) {
+            document.getElementsByClassName("musica")[i].style.animation = "none";
+        }
+        document.getElementById("rotateImage").style.animation = "none";
+    }
+    console.log(canciones);
+}
+
+setInterval(bucle, 1000);
