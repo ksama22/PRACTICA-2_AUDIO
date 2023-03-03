c943e9b4cc81513334b760335d0e8526cd0b88b6
ksama22 <ksama22@jaumebalmes.net>
2023-03-03 20:49:58 +0100

    fetch and added songs

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-var indexSong = 0;
-var audio = new Audio(canciones[indexSong]);
 
 //Inicializa los segundos y minutos
 let sec = 0;
 let min = 0;
 //Esto se llama en bucle
 function bucle() {
-    if (!audio.paused) {
+    if (!reproductor.paused) {
         goTimer();
     }
 
     //Pasa de cancion si finaliza 
-    if (audio.currentTime >= audio.duration){
+    if (reproductor.currentTime >= reproductor.duration) {
         nextMusic();
         playMusic();
     }
 }
+//Cancion actual
+var indexSong = 0;
+//Variable que tendra las canciones
+let canciones = null;
+//No uso <audio>, lo defino en new Audio
+var reproductor = null
+
+// funcion asincrona (async await)
+async function carregaDades() {
+    await fetch('./dades.json')
+        //La respuesta en formato json
+        .then(response => response.json())
+        .then(data => {
+            //El resultado (data) lo guarda en 'canciones'
+            canciones = data;
+        })
+    //El reprodcutor cargara la primera cancion
+    reproductor = new Audio(canciones[indexSong]);
+    //Escribe el titulo, la imagen y el artista
+    refreshData();
+}
+
 function goTimer() {
     //Suma segundos
     sec++;
@@ -39,40 +59,38 @@ function playMusic() {
     //document.getElementById("icon-play").classList.replace("pause_circle_filled", "play_circle_filled");
     //Pausa la animacion quan no esta reproduint
     playAnimation()
-    if (!audio.paused) {
+    if (!reproductor.paused) {
         document.getElementById("icon-play").innerText = "pause_circle_filled";
         pauseAnimation();
-        audio.pause();
+        reproductor.pause();
     } else {
         playAnimation();
         document.getElementById("icon-play").innerText = "play_circle_filled";
-        audio.play();
+        reproductor.play();
     }
-    console.log(document.getElementById("icon-play").innerText);
 }
 
 function backMusic() {
-    audio.pause();
+    reproductor.pause();
     //Para la cancion anterior
-    //audio.stop();
+    //reproductor.stop();
     //Si intenta anar una cancion mes petita retorna el minim
     if (indexSong - 1 >= 0) {
         indexSong--;
     }
-    pauseAnimation()
+    stopAnimation()
     refreshData();
 }
 
 function nextMusic() {
-    audio.pause();
+    reproductor.pause();
     //Para la cancion anterior
-    //audio.stop();
+    //reproductor.stop();
     //Si intenta anar una cancion mes gran retorna el minim
     if (indexSong + 1 < canciones.length) {
         indexSong++;
     }
-    pauseAnimation()
-
+    stopAnimation()
     refreshData();
 }
 
@@ -81,7 +99,7 @@ function refreshData() {
     document.getElementsByClassName("title")[0].innerText = cancionOBJ.titol;
     document.getElementsByClassName("artista")[0].innerText = cancionOBJ.artista;
     document.getElementById("rotateImage").src = cancionOBJ.image;
-    audio = new Audio(canciones[indexSong].path);
+    reproductor = new Audio(canciones[indexSong].path);
     //console.log("Cancion actual", cancionOBJ);
 
     // Reseteja el contador
@@ -94,8 +112,8 @@ function refreshData() {
 
 function changeVolume() {
     let a = document.getElementById("volume-input").value;
-    audio.volume = parseFloat(a) / 100;
-    console.log("volumen", a, audio.volume);
+    reproductor.volume = parseFloat(a) / 100;
+    console.log("volumen", a, reproductor.volume);
 }
 
 function playAnimation() {
@@ -110,6 +128,17 @@ function playAnimation() {
 }
 
 function pauseAnimation() {
+    //Treu la animacio per cada barra 
+    let barresMusicals = document.getElementsByClassName("musica");
+    //Si l'estat es false treu les animacions
+    for (let i = 0; i < barresMusicals.length; i++) {
+        barresMusicals[i].style.animationPlayState = "paused";
+    }
+    //Treu la animacio al la imatge per girar
+    document.getElementById("rotateImage").style.animationPlayState = "paused";
+}
+
+function stopAnimation() {
     //Treu la animacio per cada barra 
     let barresMusicals = document.getElementsByClassName("musica");
     //Si l'estat es false treu les animacions
